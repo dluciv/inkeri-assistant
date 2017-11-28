7195256c395b75316a365bf1149417539172d0c0
Dmitry V. Luciv <dluciv@dluciv.name>
2017-11-28 10:52:18 +0300

    3rd party CORS proxy

--- seals.js
+++ seals.js
@@ -7,7 +7,7 @@ const seals_default = "Ситуация с тюленями спокойная."
 const seal_back_value_template = _.template('Фон — <%= micro_tul_hour %> микро<%= declinated %> в час.');
 const seal_text_template       = _.template('<%= status_text %> <%= back_text %> Центр реабилитации морских млекопитающих Ленинградской области сообщает. <%= text %>');
 
-const seals_url = 'https://matrix.dluciv.name/vksealrescuerss';
+const seals_url = 'https://cors-anywhere.herokuapp.com/http://myfeeds.info/?2angrbmt';
 
 var seal_back_value = " ";
 var seal_status_text = "";
