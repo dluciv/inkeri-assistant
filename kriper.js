6905aebbabce425246aa2b119f78ba3dfa19576c
Nikolay Sokolov <nikolay@jamaza.com>
2017-11-28 12:05:24 +0300

    kriper: story url tracing

--- kriper.js
+++ kriper.js
@@ -3,11 +3,27 @@ import { t_ga } from './misc.js';
 // const KRIPER_URL = 'https://kriper.ru/random';
 const KRIPER_URL = 'https://cors-anywhere.herokuapp.com/https://kriper.ru/random';
 
+var getFinalUrl = function(xhr) {
+  var headers = xhr.getAllResponseHeaders().split('\n');
+  var found = _.find(headers, (h) => h.trim().startsWith('x-final-url'));
+  if (!found) {
+    return null;
+  }
+  var pair = found.split(':');
+  if (pair.length < 3) {
+    console.log('wrong x-final-url header. ', headers);
+    return null;
+  }
+  return pair[1].trim() + pair[2].trim();
+}
+
 var loadKriperStory = function(callback) {
   $.ajax({
     url: KRIPER_URL,
     method: 'GET',
-    success: function(resp) {
+    success: function(resp, result, xhr) {
+      var finalUrl = getFinalUrl(xhr);
+      t_ga('duckduckgo', 'kriper_redirect', finalUrl);
       var text = $('#content .text', resp).text();
       callback(text);
     },
