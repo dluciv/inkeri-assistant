7e928f85b0b52d2e589faade2aa7154e8bd88dd7
Nikolay Sokolov <jentalmeister@gmail.com>
2017-12-24 02:00:20 +0300

    todoist: second steps

--- settings.js
+++ settings.js
@@ -1,23 +1,57 @@
-var tdiClientId = null;
-var tdiScope    = null;
-var tdiState    = null;
+import { getUrlVars } from './misc.js';
+
+var urlVars = getUrlVars();
+console.log(urlVars);
+
+var tdiClientId     = null;
+var tdiClientSecret = null;
+var tdiScope        = null;
+var tdiState        = null;
 
 $.ajax({
   url: 'https://inkeri-api.herokuapp.com/todoist-auth-data',
   method: 'GET',
   dataType: 'json'
 })
-.then(({clientId, scope, state}) => {
-  tdiClientId = clientId;
-  tdiScope    = scope;
-  tdiState    = state;
+.then(({clientId, clientSecret, scope, state}) => {
+  tdiClientId      = clientId;
+  tdiClientSecret  = clientSecret;
+  tdiScope         = scope;
+  tdiState         = state;
 
   return $(document).ready()
 })
 .then(() => {
-  var todoistAuthUrl = `https://todoist.com/oauth/authorize?client_id=${tdiClientId}&scope=${tdiScope}&state=${tdiState}`;
-  $('#authTodoistBtn').prop('href', todoistAuthUrl);
+  if (urlVars['action'] == 'oauth_redirect' && urlVars['service'] == 'todoist') {
+    $('#authTodoistStatus').text('Authorizing...');
+
+    var authCode = urlVars['code'];
+    var redirectUri = 
+    var todoistAuthUrl = `https://todoist.com/oauth/access_token?client_id=${tdiClientId}&client_secret=${tdiClientSecret}&code=${authCode}&redirect_uri=https://inkeri.tk/settings.html`;
+
+    return $.ajax({
+      url: todoistAuthUrl,
+      method: 'GET',
+      dataType: 'json'
+    });
+  }
+  else {
+    var todoistAuthUrl = `https://todoist.com/oauth/authorize?client_id=${tdiClientId}&scope=${tdiScope}&state=${tdiState}`;
+    $('#authTodoistBtn')
+    .prop('href', todoistAuthUrl)
+    .toggleClass('uk-disabled', false);
+
+    return Promise.resolve();
+  }
+})
+.then((res) => {
+  if (res) {
+    console.log('token: ', res.access_token);
+  }
 })
 .catch((err) => {
   console.log('err: ', err);
-})
+});
+
+
+
