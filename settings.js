a4d5bf91ce215fdeeacec257a45b2c323907518b
Nikolay Sokolov <jentalmeister@gmail.com>
2018-01-01 19:35:19 +0500

    js.cookie.js

--- settings.js
+++ settings.js
@@ -1,4 +1,5 @@
 import { getUrlVars } from './misc.js';
+import * as Cookies from './thirdparty/js.cookie.js';
 
 var urlVars = getUrlVars();
 console.log(urlVars);
@@ -7,6 +8,19 @@ var tdiClientId     = null;
 var tdiClientSecret = null;
 var tdiScope        = null;
 var tdiState        = null;
+
+const TODOIST_AUTH_STATES = {
+  INITIAL  : 0,
+  REDIRECT : 1
+};
+
+var todoistAuthState = null;
+if (urlVars['action'] == 'oauth_redirect' && urlVars['service'] == 'todoist') {
+  todoistAuthState = TODOIST_AUTH_STATES.REDIRECT;
+}
+else {
+  todoistAuthState = TODOIST_AUTH_STATES.INITIAL;
+}
 
 $.ajax({
   url: 'https://inkeri-api.herokuapp.com/todoist-auth-data',
@@ -22,7 +36,15 @@ $.ajax({
   return $(document).ready()
 })
 .then(() => {
-  if (urlVars['action'] == 'oauth_redirect' && urlVars['service'] == 'todoist') {
+  if (todoistAuthState == TODOIST_AUTH_STATES.INITIAL) {
+    var todoistAuthUrl = `https://todoist.com/oauth/authorize?client_id=${tdiClientId}&scope=${tdiScope}&state=${tdiState}`;
+    $('#authTodoistBtn')
+    .prop('href', todoistAuthUrl)
+    .toggleClass('uk-disabled', false);
+
+    return Promise.resolve();
+  }
+  else if (todoistAuthState == TODOIST_AUTH_STATES.REDIRECT) {
     $('#authTodoistStatus').text('Authorizing...');
 
     var authCode = urlVars['code'];
@@ -36,17 +58,15 @@ $.ajax({
     });
   }
   else {
-    var todoistAuthUrl = `https://todoist.com/oauth/authorize?client_id=${tdiClientId}&scope=${tdiScope}&state=${tdiState}`;
-    $('#authTodoistBtn')
-    .prop('href', todoistAuthUrl)
-    .toggleClass('uk-disabled', false);
-
     return Promise.resolve();
   }
 })
 .then((res) => {
   if (res) {
     console.log('token: ', res.access_token);
+    if (res.access_token) {
+      Cookies.set('todoist_auth_token', res.access_token, 365);
+    }
   }
 })
 .catch((err) => {
