ae37bb868b9e382d261edc89f30e03f8d28d3de1
Dmitry V. Luciv <dluciv@dluciv.name>
2018-01-24 16:17:10 +0300

    Norppa fallback

--- seals.js
+++ seals.js
@@ -3,6 +3,7 @@ import { t_ga, declinateUnit, calculateWeightedAverage } from './misc.js';
 const seals_ok = "Ситуация с тюленями обнадёживающая.";
 const seals_not_ok = "Ситуация с тюленями угрожающая.";
 const seals_default = "Ситуация с тюленями спокойная.";
+const seals_unknown = "Данные по тюленям не поступили.";
 
 const seal_back_value_template = _.template('Фон — <%= micro_tul_hour %> микро<%= declinated %> в час.');
 const seal_text_template       = _.template('<%= status_text %> <%= back_text %> Центр реабилитации морских млекопитающих Ленинградской области сообщает. <%= text %>');
@@ -10,8 +11,8 @@ const seal_text_template       = _.template('<%= status_text %> <%= back_text %>
 const seals_url = 'https://cors-anywhere.herokuapp.com/http://myfeeds.info/?2angrbmt';
 
 var seal_back_value = " ";
-var seal_status_text = "";
-var seal_text = "";
+var seal_status_text = seals_unknown;
+var seal_text = seals_unknown;
 
 var measure_seal_background = function(parsed) {
   var now = new Date().getTime();
