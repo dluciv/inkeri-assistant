72af4c37f255db0a2276077e2a39b0516cdd8067
Nikolay Sokolov <jentalmeister@gmail.com>
2018-06-05 08:07:50 +0300

    Better text extractor

--- reader.js
+++ reader.js
@@ -1,10 +1,11 @@
 import { t_ga } from './misc.js';
+import { BRAINS_BASE_URL } from './settings.js';
 
 let cleanup = (pageText) => {
   return $(pageText).not('script').text();
 }
 
-let readUrl = function(url, callback) {
+let readUrlWithHeroku = function(url, callback) {
   var urlWithHeroku = 'https://cors-anywhere.herokuapp.com/' + url;
 
   $.ajax({
@@ -21,5 +22,25 @@ let readUrl = function(url, callback) {
     }
   });
 }
+
+let readUrlText = function(url, callback) {
+  var url2 = `${BRAINS_BASE_URL}urlText?url=${url}`;
+
+  $.ajax({
+    url: url2,
+    method: 'GET',
+    success: function(resp, result, xhr) {
+      window.resp = resp;
+      callback(resp.title + '\n' + cleanup(resp.content));
+    },
+    error: function(err) {
+      console.log('Error. ', err);
+      t_ga('search', 'failed to read url', err.toString());
+      callback("Ничего не нашла");
+    }
+  });
+}
+
+let readUrl = readUrlText;
 
 export { readUrl }
