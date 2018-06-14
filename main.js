3498001dcd2e784c9cd970352d25b889cb09aec4
Nikolay Sokolov <jentalmeister@gmail.com>
2018-06-14 10:47:16 +0300

    html refactoring

--- main.js
+++ main.js
@@ -10,6 +10,21 @@ import { matchInkeri, matchInkeriAny, matchStop, matchStopAny, matchNext, matchN
 import { init as initPushes, onEvent as onPushEvent } from './push.js';
 import { readUrl } from './reader.js';
 
+// -- To Force https ------------------------------
+// -- https://stackoverflow.com/a/4723302/539470 --
+if (location.protocol != 'https:' && location.hostname != 'localhost' && location.hostname != '127.0.0.1')
+  location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+// --
+
+// -- Global site tag (gtag.js) - Google Analytics --
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'UA-110108110-1');
+// --
+
 var SpeechRecognition = null;
 var SpeechGrammarList = null;
 var SpeechRecognitionEvent = null;
